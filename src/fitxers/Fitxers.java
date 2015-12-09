4b0866a628c6dfab643fa1d914a2e6ba987ce0d6
Sergi <spineda1994@gmail.com>
2015-12-09 21:28:42 +0100

    Final

---
+++ src/fitxers/Fitxers.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fitxers;
+
+
+import java.io.File;
+
+/**
+ *
+ * @author sergi
+ */
+public class Fitxers {
+
+     File f = new File("/home/sergi","xxprova");
+        
+    public void main(String[] args) {
+        int a = 1;
+    System.out.println("Fichero "+ f.canRead());   
+    
+    }
+   
+         
+}
